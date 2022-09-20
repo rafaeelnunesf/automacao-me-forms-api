ef80777631c06b838950c5f3c20e672afe55a6e0
rafaeelnunes <rafaelnfsq@gmail.com>
2022-09-20 14:27:15 -0300

    feat: create initial server config

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import "./setup.js";
+import cors from "cors";
+import express from "express";
+import "express-async-errors";
+// import authRouter from "./routers/authRouter.js";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use("/auth", authRouter);
+app.use(errorHandlerMiddleware);
+export default app;
